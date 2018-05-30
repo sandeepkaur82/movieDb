f79f0f17a090fe43afe7997da730ff7b69aa64bb
Sandeep Kaur <sandeepkaur@workfront.com>
2018-05-30 15:11:50 -0600

    Created files with more tests identified with TODOs

---
+++ src/test/java/com/sofi/moviedb/tests/CompaniesTests.java
@@ -0,0 +1,27 @@
+package com.sofi.moviedb.tests;
+
+/**
+ * Created by sandeepkaur on 5/30/18.
+ */
+public class CompaniesTests {
+
+	/**
+	 * On passing a valid company id
+	 * I should expect back a 200 status code.
+	 */
+
+	/**
+	 * On passing an invalid company id
+	 * I should expect back a 404 status code.
+	 */
+
+	/**
+	 * On passing a valid company id
+	 * I should be able to retrieve company's alternative name
+	 */
+
+	/**
+	 * On passing a valid company id
+	 * I should be able to retrieve company's logo
+	 */
+}
---
+++ src/test/java/com/sofi/moviedb/tests/MovieTests.java
@@ -0,0 +1,42 @@
+package com.sofi.moviedb.tests;
+
+/**
+ * Created by sandeepkaur on 5/30/18.
+ */
+public class MovieTests {
+
+	/**
+	 * On passing a valid movie id
+	 * I should expect back a 200 status code.
+	 */
+
+	/**
+	 * On passing a invalid movie id
+	 * I should expect back a 404 status code.
+	 */
+
+	/**
+	 * On passing a valid movie id along with a valid language string
+	 * I should expect back a 200 status code.
+	 */
+
+	/**
+	 * On passing a valid movie id along with an invalid language string
+	 * I should expect back a 404 status code.
+	 */
+
+	/**
+	 * On passing a valid movie id
+	 * I should should be able to retrieve and validate boolean adult field value.
+	 */
+
+	/**
+	 * On passing a valid movie id
+	 * I should should be able to retrieve and validate String title field value.
+	 */
+
+	/**
+	 * On passing a valid movie id
+	 * I should should be able to retrieve and validate String release_date field value.
+	 */
+}
