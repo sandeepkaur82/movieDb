158148df1631bb02a566fb52154341211e8592dc
Sandeep Kaur <sandeepkaur@workfront.com>
2018-05-31 08:24:09 -0600

    Removed apiKey

--- src/test/java/com/sofi/moviedb/constants/TestConstants.java
+++ src/test/java/com/sofi/moviedb/constants/TestConstants.java
@@ -5,7 +5,7 @@ package com.sofi.moviedb.constants;
  */
 public class TestConstants {
 
-	public static final String apiKey = "6cc33a375d9e1424b569519006cc9018";
+	public static final String apiKey = "";
 	public static final String baseURL = "https://api.themoviedb.org/";
 	public static final String generateTokenURL = String.format("3/authentication/token/new?api_key=%s", apiKey);
 
